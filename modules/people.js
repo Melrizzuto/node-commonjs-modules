6a59048c13b523470dd16c60841ebf662549a644
Melrizzuto <melrizzuto4@gmail.com>
2024-11-29 17:14:45 +0100

    added comments people js

--- modules/people.js
+++ modules/people.js
@@ -2,12 +2,13 @@ const createName = require('./names'); // Importo names.js
 const createHobbies = require('./hobbies'); // Importo hobbies.js
 
 function myPerson() {
-    // Uso la funzione createName per generare un oggetto fullName
+    //variabili per inserire gli args da console
     const firstName = process.argv[2] ?? "firstname";
     const lastName = process.argv[3] ?? "lastname";
     const hobbyOne = process.argv[4] ?? "hobby 1";
     const hobbyTwo = process.argv[5] ?? "hobby 2";
     const hobbyThree = process.argv[6] ?? "hobby 3";
+    // Uso la funzione createName per generare un oggetto fullName
     const fullName = createName(firstName, lastName)
 
     // Uso la funzione createHobbies per generare un oggetto con hobbies
