59f4296902e4d09fda5ac7c04f74fe0e71d16827
Melrizzuto <melrizzuto4@gmail.com>
2024-11-29 15:36:34 +0100

    created fn in people.js and added letter "s" to dir name.js (:P ops)

--- modules/people.js
+++ modules/people.js
@@ -1,2 +1,15 @@
 const createName = require('./names'); // Importo names.js
 const createHobbies = require('./hobbies'); // Importo hobbies.js
+
+function myPerson() {
+    // Uso la funzione createName per generare un oggetto fullName
+    const fullName = createName(firstName, lastName);
+
+    // Uso la funzione createHobbies per generare un oggetto con hobbies
+    const hobbies = createHobbies(hobbyOne, hobbyTwo, hobbyThree);
+
+    // Combino fullName e hobbies in un unico oggetto
+    return { fullName, hobbies };
+}
+
+module.exports = myPerson;
